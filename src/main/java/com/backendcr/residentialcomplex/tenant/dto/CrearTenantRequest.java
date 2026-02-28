e421436368dbaaae4a410c53dde38e32f4e017b2
Brainer <brainer.sinning@aspsols.com>
2026-02-27 22:03:26 -0500

    Update CrearTenantRequest.java
    Integracion de modelo Create Tenant de tipo Record

--- src/main/java/com/backendcr/residentialcomplex/tenant/dto/CrearTenantRequest.java
+++ src/main/java/com/backendcr/residentialcomplex/tenant/dto/CrearTenantRequest.java
@@ -1,24 +1,9 @@
 package com.backendcr.residentialcomplex.tenant.dto;
 
-public class CrearTenantRequest {
-
-	private String schemaName;
-	private String nombre;
-
-	public String getSchemaName() {
-		return schemaName;
-	}
-
-	public void setSchemaName(String schemaName) {
-		this.schemaName = schemaName;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+public record CrearTenantRequest (
+		String schemaName,
+		String nombre,
+		String codigo
+) {
 
 }
