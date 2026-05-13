687d8b1ee26f92473d8bb1c8d2ac6c443949de20
Bray <brayner2112@gmail.com>
2026-05-13 08:34:53 -0500

    Update DataInitializer.java

--- src/main/java/com/backendcr/residentialcomplex/config/DataInitializer.java
+++ src/main/java/com/backendcr/residentialcomplex/config/DataInitializer.java
@@ -29,7 +29,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
-@Profile("dev")
+@Profile("default")
 @RequiredArgsConstructor
 public class DataInitializer implements CommandLineRunner {
 
